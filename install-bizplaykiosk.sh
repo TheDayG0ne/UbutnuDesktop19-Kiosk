1c13562269b412c68614360f437b07aa9d04443c
hugGITlesbontuyaux <34125504+hugGITlesbontuyaux@users.noreply.github.com>
2019-09-16 12:50:30 +0200

    install-bizplaykiosk.sh
    install and prepare for kiosk player under Ubuntu 19.04

---
+++ install-bizplaykiosk.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# update ubuntu
+apt-get update && apt-get upgrade
+
+# get software
+sudo apt-get install \
+  unclutter \
+  chromium \
+  ssh \
+  -y
+
+# dir
+sudo mkdir -p /home/kiosk/.config/autostart
+
+# create kiosk autostart desktop
+if [ -e "/home/kiosk/.config/autostart/kiosk.desktop" ]; then
+  mv /home/kiosk/.config/autostart/kiosk.desktop /home/kiosk/.config/autostart/kiosk.desktop.backup
+fi
+cat > /home/kiosk/.config/autostart/kiosk.desktop << EOF
+[Desktop Entry]
+Type=Application
+Name=chromium
+Exec=chromium-browser --password-store=basic --no-first-run --disable --disable-translate --disable-infobars --disable-suggestions-service --disable-save-password-bubble --disable-features=InfiniteSessionRestore --start-maximized --kiosk "http://play.playr.biz"
+X-GNOME-Autostart-enabled=true
+
+EOF
+
+echo "Done!"
